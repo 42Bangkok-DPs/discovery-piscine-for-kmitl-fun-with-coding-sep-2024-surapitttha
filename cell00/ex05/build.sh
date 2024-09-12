c7fd267aa98d4eedc3edee70e59b1509d38effc7
surapitttha <110759409+surapitttha@users.noreply.github.com>
2024-09-12 19:36:04 +0700

    no

--- cell00/ex05/build.sh
+++ cell00/ex05/build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# ตรวจสอบว่ามีการส่ง arguments มาหรือไม่
+
 if [ $# -eq 0 ]; then
   echo "No arguments supplied"
   exit 1
 fi
 
-# วนลูปผ่าน arguments ที่ส่งมา
+
 for arg in "$@"; do
   folder_name="ex$arg"
-  # สร้างโฟลเดอร์ถ้ายังไม่มี
+
   if [ ! -d "$folder_name" ]; then
     mkdir "$folder_name"
     echo "Directory $folder_name created"
